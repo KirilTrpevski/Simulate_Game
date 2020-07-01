ceef6c29ae8ac26132a65e380e63774e12233f76
Kiril Trpevski <kiril.trpevski@gmail.com>
2020-07-01 18:32:34 +0200

    RectangleShape class added

---
+++ Simulate_Game/RectangleShape.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulate_Game
+{
+   public  class RectangleShape
+    {
+        public Point Location { get; set; }
+        public Color Color { get; set; }
+        public bool isSelected { get; set; }
+        public RectangleShape(Point Location, Color Color)
+        {
+            this.Location = Location;
+            this.Color = Color;
+            isSelected = false;
+        }
+        public void Draw(Graphics g){}
+        public void Clicked(Point Lokacija) { }
+    }
+}
