9f3b19465c77b21fa249d6925c5866377f1016ed
Daxonny <50597891+Daxonny@users.noreply.github.com>
2020-07-03 17:29:24 +0200

    Added buttons

--- Simulate_Game/Form1.Designer.cs
+++ Simulate_Game/Form1.Designer.cs
@@ -28,13 +28,85 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnRed3 = new System.Windows.Forms.Button();
+            this.btnGreen4 = new System.Windows.Forms.Button();
+            this.btnBlue2 = new System.Windows.Forms.Button();
+            this.btnOrange1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnRed3
+            // 
+            this.btnRed3.BackColor = System.Drawing.Color.DarkRed;
+            this.btnRed3.Location = new System.Drawing.Point(215, 268);
+            this.btnRed3.Name = "btnRed3";
+            this.btnRed3.Size = new System.Drawing.Size(224, 195);
+            this.btnRed3.TabIndex = 2;
+            this.btnRed3.UseVisualStyleBackColor = false;
+            // 
+            // btnGreen4
+            // 
+            this.btnGreen4.BackColor = System.Drawing.Color.Green;
+            this.btnGreen4.Location = new System.Drawing.Point(484, 268);
+            this.btnGreen4.Name = "btnGreen4";
+            this.btnGreen4.Size = new System.Drawing.Size(224, 195);
+            this.btnGreen4.TabIndex = 3;
+            this.btnGreen4.UseVisualStyleBackColor = false;
+            // 
+            // btnBlue2
+            // 
+            this.btnBlue2.BackColor = System.Drawing.Color.RoyalBlue;
+            this.btnBlue2.Location = new System.Drawing.Point(484, 54);
+            this.btnBlue2.Name = "btnBlue2";
+            this.btnBlue2.Size = new System.Drawing.Size(224, 195);
+            this.btnBlue2.TabIndex = 4;
+            this.btnBlue2.UseVisualStyleBackColor = false;
+            this.btnBlue2.Click += new System.EventHandler(this.btnBlue2_Click);
+            // 
+            // btnOrange1
+            // 
+            this.btnOrange1.BackColor = System.Drawing.Color.DarkOrange;
+            this.btnOrange1.Location = new System.Drawing.Point(215, 54);
+            this.btnOrange1.Name = "btnOrange1";
+            this.btnOrange1.Size = new System.Drawing.Size(224, 195);
+            this.btnOrange1.TabIndex = 5;
+            this.btnOrange1.UseVisualStyleBackColor = false;
+            this.btnOrange1.Click += new System.EventHandler(this.btnOrange1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Items.AddRange(new object[] {
+            "asd"});
+            this.listBox1.Location = new System.Drawing.Point(26, 82);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 84);
+            this.listBox1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(945, 519);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnOrange1);
+            this.Controls.Add(this.btnBlue2);
+            this.Controls.Add(this.btnGreen4);
+            this.Controls.Add(this.btnRed3);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnRed3;
+        private System.Windows.Forms.Button btnGreen4;
+        private System.Windows.Forms.Button btnBlue2;
+        private System.Windows.Forms.Button btnOrange1;
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
 
--- Simulate_Game/Form1.cs
+++ Simulate_Game/Form1.cs
@@ -12,9 +12,50 @@ namespace Simulate_Game
 {
     public partial class Form1 : Form
     {
+        
         public Form1()
         {
             InitializeComponent();
         }
+        String[] btns = { "btnOrange1", "btnBlue2", "btnRed3", "btnGreen4" };
+        List<string> seq = new List<string>();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //foreach (string s in btns)
+            //{
+            //    listBox1.Items.Add(s);
+            //}
+            for (int i = 0; i <= 2; i++)
+            {
+                btnBlue2.PerformClick();
+            }
+            var seq = new List<string>();
+
+        }
+        
+        
+
+        //Random rand = new Random();
+
+        private void btnOrange1_Click(object sender, EventArgs e)
+        {
+            btnBlue2.PerformClick();
+            listBox1.Items.Add(btns[0]);
+            seq.Add(btns[0]);
+            foreach (string i in seq)
+            {
+                MessageBox.Show(i);
+            }
+
+        }
+
+        private void btnBlue2_Click(object sender, EventArgs e)
+        {
+            btnBlue2.BackColor = Color.Red;
+        }
+
+
     }
+
 }
