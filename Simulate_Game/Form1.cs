15711e884ade6920e4e56ea3ab15cbc7744285f2
Kiril Trpevski <kiril.trpevski@gmail.com>
2020-07-05 23:09:41 +0200

    Fixed random elements

--- Simulate_Game/Form1.cs
+++ Simulate_Game/Form1.cs
@@ -19,25 +19,29 @@ namespace Simulate_Game
         {
             InitializeComponent();
             btn1.Location = new Point(250, 50);
-            btn1.Name = "btn1";
+            btn1.Name = "btn0";
             btn1.Height = 180;
             btn1.Width = 180;
             btn1.BackColor = Color.RoyalBlue;
+            btn1.Text = "kopce0";
             btn2.Location = new Point(450, 50);
-            btn2.Name = "btn2";
+            btn2.Name = "btn1";
             btn2.Height = 180;
             btn2.Width = 180;
             btn2.BackColor = Color.RoyalBlue;
-            btn3.Location = new Point(450, 250);
-            btn3.Name = "btn3";
+            btn2.Text = "kopce1";
+            btn3.Location = new Point(250, 250);
+            btn3.Name = "btn2";
             btn3.Height = 180;
             btn3.Width = 180;
             btn3.BackColor = Color.RoyalBlue;
-            btn4.Location = new Point(250, 250);
-            btn4.Name = "btn4";
+            btn3.Text = "kopce2";
+            btn4.Location = new Point(450, 250);
+            btn4.Name = "btn3";
             btn4.Height = 180;
             btn4.Width = 180;
             btn4.BackColor = Color.RoyalBlue;
+            btn4.Text = "kopce3";
             Controls.Add(btn1);
             Controls.Add(btn2);
             Controls.Add(btn3);
@@ -56,49 +60,44 @@ namespace Simulate_Game
         Button btn3 = new Button();
         Button btn4 = new Button();
 
-        
+        int level = 4;
 
         private void Form1_Load(object sender, EventArgs e)
         {
             
         }
-
         private void shuffleButtons()
         {
             Random random = new Random();
-            var randomNumbers = Enumerable.Range(0, 4).OrderBy(x => random.Next()).Take(4).ToList();
-            int[] n = new int[4];
-            List<int> a = new List<int>();
+            List<int> randomNumbers = new List<int>();
+
+            for(int i = 0; i<level; i++)
+            {
+                randomNumbers.Add(random.Next(0, 4));
+            }
+            List<Button> a = new List<Button>();
             foreach (int item in randomNumbers)
             {
-                a.Add(item);
+                a.Add(btns[item]);
             }
-            for (int i = 0; i < btns.Count; i++)
-            {
-                n[i] = a[i];
-                btns[i] = btns[n[i]];
-            }
-            foreach(var j in a)
-            {
-                MessageBox.Show(j.ToString());
-            }
+            btns = a;
         }
 
         private async void startTimer_Click(object sender, EventArgs e)
         {
             listBox1.Items.Clear();
             shuffleButtons();
-          foreach(Button b in btns)
+            foreach (Button b in btns)
             {
+                //MessageBox.Show(b.Name);
                 b.BackColor = Color.RoyalBlue;
                 await Task.Delay(400);
                 b.BackColor = Color.Transparent;
                 await Task.Delay(1000);
                 b.BackColor = Color.RoyalBlue;
-               
+
                 listBox1.Items.Add(b.Name);
             }
-            
         }
     }
        
