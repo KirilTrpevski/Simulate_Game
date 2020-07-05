e20d7d430f4bf312ca28a71f21ff72a0578da66d
Kiril Trpevski <kiril.trpevski@gmail.com>
2020-07-05 21:50:09 +0200

    Dinamicaly Added buttons

--- Simulate_Game/Form1.Designer.cs
+++ Simulate_Game/Form1.Designer.cs
@@ -29,10 +29,6 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.btnRed3 = new System.Windows.Forms.Button();
-            this.btnGreen4 = new System.Windows.Forms.Button();
-            this.btnBlue2 = new System.Windows.Forms.Button();
-            this.btnOrange1 = new System.Windows.Forms.Button();
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.startTimer = new System.Windows.Forms.Button();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
@@ -40,45 +36,6 @@
             this.timer2 = new System.Windows.Forms.Timer(this.components);
             this.timer3 = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
-            // 
-            // btnRed3
-            // 
-            this.btnRed3.BackColor = System.Drawing.Color.DarkRed;
-            this.btnRed3.Location = new System.Drawing.Point(215, 268);
-            this.btnRed3.Name = "btnRed3";
-            this.btnRed3.Size = new System.Drawing.Size(224, 195);
-            this.btnRed3.TabIndex = 2;
-            this.btnRed3.UseVisualStyleBackColor = false;
-            this.btnRed3.Click += new System.EventHandler(this.btnRed3_Click);
-            // 
-            // btnGreen4
-            // 
-            this.btnGreen4.BackColor = System.Drawing.Color.Green;
-            this.btnGreen4.Location = new System.Drawing.Point(484, 268);
-            this.btnGreen4.Name = "btnGreen4";
-            this.btnGreen4.Size = new System.Drawing.Size(224, 195);
-            this.btnGreen4.TabIndex = 3;
-            this.btnGreen4.UseVisualStyleBackColor = false;
-            // 
-            // btnBlue2
-            // 
-            this.btnBlue2.BackColor = System.Drawing.Color.RoyalBlue;
-            this.btnBlue2.Location = new System.Drawing.Point(484, 67);
-            this.btnBlue2.Name = "btnBlue2";
-            this.btnBlue2.Size = new System.Drawing.Size(224, 195);
-            this.btnBlue2.TabIndex = 4;
-            this.btnBlue2.UseVisualStyleBackColor = false;
-            this.btnBlue2.Click += new System.EventHandler(this.btnBlue2_Click);
-            // 
-            // btnOrange1
-            // 
-            this.btnOrange1.BackColor = System.Drawing.Color.DarkOrange;
-            this.btnOrange1.Location = new System.Drawing.Point(215, 67);
-            this.btnOrange1.Name = "btnOrange1";
-            this.btnOrange1.Size = new System.Drawing.Size(224, 195);
-            this.btnOrange1.TabIndex = 5;
-            this.btnOrange1.UseVisualStyleBackColor = false;
-            this.btnOrange1.Click += new System.EventHandler(this.btnOrange1_Click);
             // 
             // listBox1
             // 
@@ -92,9 +49,9 @@
             // 
             // startTimer
             // 
-            this.startTimer.Location = new System.Drawing.Point(819, 67);
+            this.startTimer.Location = new System.Drawing.Point(746, 50);
             this.startTimer.Name = "startTimer";
-            this.startTimer.Size = new System.Drawing.Size(75, 23);
+            this.startTimer.Size = new System.Drawing.Size(175, 45);
             this.startTimer.TabIndex = 7;
             this.startTimer.Text = "Start";
             this.startTimer.UseVisualStyleBackColor = true;
@@ -102,8 +59,7 @@
             // 
             // timer1
             // 
-            this.timer1.Interval = 500;
-            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            this.timer1.Interval = 1000;
             // 
             // timerResult
             // 
@@ -121,10 +77,6 @@
             this.Controls.Add(this.timerResult);
             this.Controls.Add(this.startTimer);
             this.Controls.Add(this.listBox1);
-            this.Controls.Add(this.btnOrange1);
-            this.Controls.Add(this.btnBlue2);
-            this.Controls.Add(this.btnGreen4);
-            this.Controls.Add(this.btnRed3);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -133,12 +85,6 @@
         }
 
         #endregion
-
-
-        private System.Windows.Forms.Button btnRed3;
-        private System.Windows.Forms.Button btnGreen4;
-        private System.Windows.Forms.Button btnBlue2;
-        private System.Windows.Forms.Button btnOrange1;
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Button startTimer;
         private System.Windows.Forms.Timer timer1;
--- Simulate_Game/Form1.cs
+++ Simulate_Game/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,93 +14,93 @@ namespace Simulate_Game
 {
     public partial class Form1 : Form
     {
-        
+
         public Form1()
         {
             InitializeComponent();
-        }
-        List<string> btns = new List<string> { "btnOrange1", "btnBlue2", "btnRed3", "btnGreen4" };
-        List<string> clicked = new List<string>(); 
-        List<string> seq = new List<string>();
-        int test;
+            btn1.Location = new Point(250, 50);
+            btn1.Name = "btn1";
+            btn1.Height = 180;
+            btn1.Width = 180;
+            btn1.BackColor = Color.RoyalBlue;
+            btn2.Location = new Point(450, 50);
+            btn2.Name = "btn2";
+            btn2.Height = 180;
+            btn2.Width = 180;
+            btn2.BackColor = Color.RoyalBlue;
+            btn3.Location = new Point(450, 250);
+            btn3.Name = "btn3";
+            btn3.Height = 180;
+            btn3.Width = 180;
+            btn3.BackColor = Color.RoyalBlue;
+            btn4.Location = new Point(250, 250);
+            btn4.Name = "btn4";
+            btn4.Height = 180;
+            btn4.Width = 180;
+            btn4.BackColor = Color.RoyalBlue;
+            Controls.Add(btn1);
+            Controls.Add(btn2);
+            Controls.Add(btn3);
+            Controls.Add(btn4);
 
-        int counter = 60;
+            btns.Add(btn1);
+            btns.Add(btn2);
+            btns.Add(btn3);
+            btns.Add(btn4);
+        }
+        List<Button> btns = new List<Button> {  };
+     
+
+        Button btn1 = new Button();
+        Button btn2 = new Button();
+        Button btn3 = new Button();
+        Button btn4 = new Button();
+
+        
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            //foreach (string s in btns)
-            //{
-            //    listBox1.Items.Add(s);
-            //}
-            for (int i = 0; i <= 2; i++)
-            {
-                btnBlue2.PerformClick();
-            }
-            var seq = new List<string>();
             
-            
-      
-
-        }
-        
-        
-
-        //Random rand = new Random();
-
-        private void btnOrange1_Click(object sender, EventArgs e)
-        {
-            btnBlue2.PerformClick();
-            //listBox1.Items.Add(btns[0]);
-            //seq.Add(btns[0]);
-            foreach (string i in clicked)
-            {
-                MessageBox.Show(i);
-            }
-
         }
 
-        private void btnBlue2_Click(object sender, EventArgs e)
-        {
-            btnBlue2.BackColor = Color.Red;
-            MessageBox.Show(btns[test]);
-        }
-
-        private void startTimer_Click(object sender, EventArgs e)
-        {
-            timer1.Interval = 500;
-            timer1.Start();
-        }
-
-        private void timer1_Tick_1(object sender, EventArgs e)
-        {
-            if(btnOrange1.BackColor == Color.DarkOrange)
-            {
-                btnOrange1.BackColor = Color.Transparent;
-            }
-            else
-            {
-                btnOrange1.BackColor = Color.DarkOrange;
-                timer1.Stop();
-                clicked.Add(btnOrange1.Name);
-            }
-
-        }
-
-        private void btnRed3_Click(object sender, EventArgs e)
+        private void shuffleButtons()
         {
             Random random = new Random();
-            for (int k = 0; k < 1; k++)
+            var randomNumbers = Enumerable.Range(0, 4).OrderBy(x => random.Next()).Take(4).ToList();
+            int[] n = new int[4];
+            List<int> a = new List<int>();
+            foreach (int item in randomNumbers)
             {
-                test = random.Next(btns.Count);
-                seq.Add(btns[test]);
-                
+                a.Add(item);
             }
-            foreach(string i in seq)
+            for (int i = 0; i < btns.Count; i++)
             {
-                listBox1.Items.Add(i);
+                n[i] = a[i];
+                btns[i] = btns[n[i]];
+            }
+            foreach(var j in a)
+            {
+                MessageBox.Show(j.ToString());
+            }
+        }
+
+        private async void startTimer_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            shuffleButtons();
+          foreach(Button b in btns)
+            {
+                b.BackColor = Color.RoyalBlue;
+                await Task.Delay(400);
+                b.BackColor = Color.Transparent;
+                await Task.Delay(1000);
+                b.BackColor = Color.RoyalBlue;
+               
+                listBox1.Items.Add(b.Name);
             }
             
         }
     }
+       
 
 }
