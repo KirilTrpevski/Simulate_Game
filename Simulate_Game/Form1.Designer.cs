fda364ba922ee93eadf3a397af544782a1ad4774
Daxonny <50597891+Daxonny@users.noreply.github.com>
2020-07-06 16:55:47 +0200

    Updated

--- Simulate_Game/Form1.Designer.cs
+++ Simulate_Game/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Simulate_Game
 {
-    partial class Form1
+    partial class timerLabel
     {
         /// <summary>
         /// Required designer variable.
@@ -29,29 +29,20 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.listBox1 = new System.Windows.Forms.ListBox();
             this.startTimer = new System.Windows.Forms.Button();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.timerResult = new System.Windows.Forms.Label();
-            this.timer2 = new System.Windows.Forms.Timer(this.components);
-            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.Poeni = new System.Windows.Forms.Label();
+            this.timeElapsed = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // listBox1
-            // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.Items.AddRange(new object[] {
-            "asd"});
-            this.listBox1.Location = new System.Drawing.Point(26, 82);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(120, 82);
-            this.listBox1.TabIndex = 6;
             // 
             // startTimer
             // 
-            this.startTimer.Location = new System.Drawing.Point(746, 50);
+            this.startTimer.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startTimer.Location = new System.Drawing.Point(964, 308);
+            this.startTimer.Margin = new System.Windows.Forms.Padding(4);
             this.startTimer.Name = "startTimer";
-            this.startTimer.Size = new System.Drawing.Size(175, 45);
+            this.startTimer.Size = new System.Drawing.Size(233, 55);
             this.startTimer.TabIndex = 7;
             this.startTimer.Text = "Start";
             this.startTimer.UseVisualStyleBackColor = true;
@@ -60,37 +51,60 @@
             // timer1
             // 
             this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // timerResult
             // 
             this.timerResult.AutoSize = true;
-            this.timerResult.Location = new System.Drawing.Point(819, 120);
+            this.timerResult.Location = new System.Drawing.Point(1092, 148);
+            this.timerResult.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.timerResult.Name = "timerResult";
-            this.timerResult.Size = new System.Drawing.Size(0, 13);
+            this.timerResult.Size = new System.Drawing.Size(0, 17);
             this.timerResult.TabIndex = 8;
             // 
-            // Form1
+            // Poeni
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.Poeni.AutoSize = true;
+            this.Poeni.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Poeni.Location = new System.Drawing.Point(964, 101);
+            this.Poeni.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Poeni.Name = "Poeni";
+            this.Poeni.Size = new System.Drawing.Size(188, 46);
+            this.Poeni.TabIndex = 9;
+            this.Poeni.Text = "Поени: 0";
+            // 
+            // timeElapsed
+            // 
+            this.timeElapsed.AutoSize = true;
+            this.timeElapsed.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timeElapsed.Location = new System.Drawing.Point(1152, 11);
+            this.timeElapsed.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.timeElapsed.Name = "timeElapsed";
+            this.timeElapsed.Size = new System.Drawing.Size(0, 25);
+            this.timeElapsed.TabIndex = 10;
+            // 
+            // timerLabel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(933, 476);
+            this.BackColor = System.Drawing.Color.PeachPuff;
+            this.ClientSize = new System.Drawing.Size(1244, 586);
+            this.Controls.Add(this.timeElapsed);
+            this.Controls.Add(this.Poeni);
             this.Controls.Add(this.timerResult);
             this.Controls.Add(this.startTimer);
-            this.Controls.Add(this.listBox1);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "timerLabel";
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Button startTimer;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Label timerResult;
-        private System.Windows.Forms.Timer timer2;
-        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Label Poeni;
+        private System.Windows.Forms.Label timeElapsed;
     }
 }
-
--- Simulate_Game/Form1.cs
+++ Simulate_Game/Form1.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -12,36 +14,36 @@ using System.Windows.Forms;
 
 namespace Simulate_Game
 {
-    public partial class Form1 : Form
+    public partial class timerLabel : Form
     {
 
-        public Form1()
+        public timerLabel()
         {
             InitializeComponent();
             btn1.Location = new Point(250, 50);
             btn1.Name = "btn0";
             btn1.Height = 180;
             btn1.Width = 180;
-            btn1.BackColor = Color.RoyalBlue;
-            btn1.Text = "kopce0";
+            btn1.BackColor = Color.Orange;
+            btn1.Click += new EventHandler(btn1_Click);
             btn2.Location = new Point(450, 50);
             btn2.Name = "btn1";
             btn2.Height = 180;
             btn2.Width = 180;
             btn2.BackColor = Color.RoyalBlue;
-            btn2.Text = "kopce1";
+            btn2.Click += new EventHandler(btn2_Click);
             btn3.Location = new Point(250, 250);
             btn3.Name = "btn2";
             btn3.Height = 180;
             btn3.Width = 180;
-            btn3.BackColor = Color.RoyalBlue;
-            btn3.Text = "kopce2";
+            btn3.BackColor = Color.Red;
+            btn3.Click += new EventHandler(btn3_Click);
             btn4.Location = new Point(450, 250);
             btn4.Name = "btn3";
             btn4.Height = 180;
             btn4.Width = 180;
-            btn4.BackColor = Color.RoyalBlue;
-            btn4.Text = "kopce3";
+            btn4.BackColor = Color.Green;
+            btn4.Click += new EventHandler(btn4_Click);
             Controls.Add(btn1);
             Controls.Add(btn2);
             Controls.Add(btn3);
@@ -52,8 +54,6 @@ namespace Simulate_Game
             btns.Add(btn3);
             btns.Add(btn4);
         }
-        List<Button> btns = new List<Button> {  };
-     
 
         Button btn1 = new Button();
         Button btn2 = new Button();
@@ -61,17 +61,200 @@ namespace Simulate_Game
         Button btn4 = new Button();
 
         int level = 4;
+        int counter = 1;
+        int score = 0;
+        int time = 12;
 
+        List<Button> btns = new List<Button> { };
+        List<Button> pom = new List<Button>() { };
+
+        List<string> checkSeq = new List<string>();
+        List<string> realSeq = new List<string>();
+        Random random = new Random();
         private void Form1_Load(object sender, EventArgs e)
         {
-            
+
+        }
+
+        public void btn1_Click(object sender, EventArgs e)
+        {
+            if (counter < level)
+            {
+                checkSeq.Add("btn1");
+            }
+            else if (counter == level)
+            {
+                for (int i = 1; i < level; i++)
+                {
+                    if (checkSeq[i] == realSeq[i])
+                    {
+                        timer1.Stop();
+                        score += 5;
+                        Poeni.Text = String.Format("Поени: {0}", score);
+                        DialogResult result = MessageBox.Show("Дали сакате да продолжите кон следното ниво? Освоивте" + Poeni.Text + " Доколку се откажете сега ќе треба да играте од почеток.",
+                            "Нивото заврши", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                        if (result == DialogResult.Yes)
+                        {
+                            level += 1;
+                            time += 4;
+                            //startTimer.PerformClick();
+                        }
+                        if (result == DialogResult.No)
+                        {
+                            level = 4;
+                            score = 0;
+                            Poeni.Text = String.Format("Поени: 0");
+                        }
+                        break;
+                    }
+                    else
+                    {
+                        timer1.Stop();
+                        time = 12;
+                        level = 4;
+                        MessageBox.Show("Game over");
+                        break;
+                    }
+                }
+            }
+            counter++;
+        }
+        public void btn2_Click(object sender, EventArgs e)
+        {
+            if (counter < level)
+            {
+                checkSeq.Add("btn1");
+            }
+            else if (counter == level)
+            {
+                for (int i = 1; i < level; i++)
+                {
+                    if (checkSeq[i] == realSeq[i])
+                    {
+                        timer1.Stop();
+                        score += 5;
+                        Poeni.Text = String.Format("Поени: {0}", score);
+                        DialogResult result = MessageBox.Show("Дали сакате да продолжите кон следното ниво? Освоивте" + Poeni.Text + " Доколку се откажете сега ќе треба да играте од почеток.",
+                            "Нивото заврши", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                        if (result == DialogResult.Yes)
+                        {
+                            level += 1;
+                            time += 4;
+                            //startTimer.PerformClick();
+                        }
+                        if (result == DialogResult.No)
+                        {
+                            level = 4;
+                            score = 0;
+                            Poeni.Text = String.Format("Поени: 0");
+                        }
+                        break;
+                    }
+                    else
+                    {
+                        timer1.Stop();
+                        time = 12;
+                        level = 4;
+                        MessageBox.Show("Game over");
+                        break;
+                    }
+                }
+            }
+            counter++;
+        }
+        public void btn3_Click(object sender, EventArgs e)
+        {
+            if (counter < level)
+            {
+                checkSeq.Add("btn2");
+            }
+            else if (counter == level)
+            {
+                for (int i = 1; i < level; i++)
+                {
+                    if (checkSeq[i] == realSeq[i])
+                    {
+                        timer1.Stop();
+                        score += 5;
+                        Poeni.Text = String.Format("Поени: {0}", score);
+                        DialogResult result = MessageBox.Show("Дали сакате да продолжите кон следното ниво? Освоивте" + Poeni.Text + " Доколку се откажете сега ќе треба да играте од почеток.",
+                            "Нивото заврши", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                        if (result == DialogResult.Yes)
+                        {
+                            level += 1;
+                            time += 4;
+                            //startTimer.PerformClick();
+                        }
+                        if (result == DialogResult.No)
+                        {
+                            level = 4;
+                            score = 0;
+                            Poeni.Text = String.Format("Поени: 0");
+                        }
+                        break;
+                    }
+                    else
+                    {
+                        timer1.Stop();
+                        time = 12;
+                        level = 4;
+                        MessageBox.Show("Game over");
+                        break;
+                    }
+                }
+            }
+            counter++;
+        }
+        public void btn4_Click(object sender, EventArgs e)
+        {
+            if (counter < level)
+            {
+                checkSeq.Add("btn3");
+            }
+            else if (counter == level)
+            {
+                for (int i = 1; i < level; i++)
+                {
+                    if (checkSeq[i] == realSeq[i])
+                    {
+                        timer1.Stop();
+                        score += 5;
+                        Poeni.Text = String.Format("Поени: {0}", score);
+                        DialogResult result = MessageBox.Show("Дали сакате да продолжите кон следното ниво? Освоивте" + Poeni.Text + " Доколку се откажете сега ќе треба да играте од почеток.",
+                            "Нивото заврши", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                        if (result == DialogResult.Yes)
+                        {
+                            level += 1;
+                            time += 4;
+                            //startTimer.PerformClick();
+                        }
+                        if (result == DialogResult.No)
+                        {
+                            level = 4;
+                            score = 0;
+                            Poeni.Text = String.Format("Поени: 0");
+                        }
+                        break;
+                    }
+                    else
+                    {
+                        timer1.Stop();
+                        time = 12;
+                        level = 4;
+                        DialogResult result = MessageBox.Show("Game over", "Играта заврши", MessageBoxButtons.OK);
+                        break;
+                    }
+                }
+
+            }
+            counter++;
         }
         private void shuffleButtons()
         {
-            Random random = new Random();
+
             List<int> randomNumbers = new List<int>();
 
-            for(int i = 0; i<level; i++)
+            for (int i = 0; i < level; i++)
             {
                 randomNumbers.Add(random.Next(0, 4));
             }
@@ -80,26 +263,51 @@ namespace Simulate_Game
             {
                 a.Add(btns[item]);
             }
+            pom = btns;
             btns = a;
         }
 
         private async void startTimer_Click(object sender, EventArgs e)
         {
-            listBox1.Items.Clear();
+            realSeq.Clear();
+            checkSeq.Clear();
+            counter = 1;
             shuffleButtons();
+            Color c;
             foreach (Button b in btns)
             {
-                //MessageBox.Show(b.Name);
-                b.BackColor = Color.RoyalBlue;
+                c = b.BackColor;
+                //b.BackColor = Color.RoyalBlue;
                 await Task.Delay(400);
                 b.BackColor = Color.Transparent;
                 await Task.Delay(1000);
-                b.BackColor = Color.RoyalBlue;
+                b.BackColor = c;
 
-                listBox1.Items.Add(b.Name);
+                realSeq.Add(b.Name);
             }
+            btns = pom;
+            timer1.Start();
         }
-    }
-       
 
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (time <= 0)
+            {
+                timer1.Stop();
+                MessageBox.Show("Game over");
+            }
+            if (time <= 5)
+            {
+                timeElapsed.ForeColor = Color.Red;
+            }
+            if (time > 5)
+            {
+                timeElapsed.ForeColor = Color.Black;
+            }
+            timeElapsed.Text = time.ToString();
+            time--;
+        }
+
+
+    }
 }
